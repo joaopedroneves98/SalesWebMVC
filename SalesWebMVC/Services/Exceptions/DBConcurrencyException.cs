137e1001cffd9e95931d2c1cc83c1dadd26b51a9
VASCONCELOS NEVES João Pedro <joaopedro.vasconcelosneves@altran.com>
2020-03-31 15:20:16 +0100

    Added update Seller function and custom service exceptions

---
+++ SalesWebMVC/Services/Exceptions/DBConcurrencyException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Services.Exceptions
+{
+    public class DBConcurrencyException : ApplicationException
+    {
+        public DBConcurrencyException(string message) : base(message) { }
+    }
+}
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
 using SalesWebMVC.Models;
+using SalesWebMVC.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -38,6 +39,23 @@ namespace SalesWebMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id Not Found!");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw new DBConcurrencyException(ex.Message);
+            }
         }
     }
 }
